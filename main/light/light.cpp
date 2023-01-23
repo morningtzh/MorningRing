7c6aeb0a4f6cd55d278a5628f85a8479a7138cc8
MorningTZH <morningtzh@yeah.net>
2023-01-23 15:42:49 +0800

    SPI点亮圆环，效果可以了。

--- main/light/light.cpp
+++ main/light/light.cpp
@@ -2,22 +2,21 @@
 // Created by MorningTZH on 2023/1/20.
 //
 
-#include <memory>
 #include "light.hpp"
+
+#include <memory>
+
+extern "C" {
+#include "SPI_ws2812.h"
+}
 
 static const char *MODULE = "Light";
 
 namespace light {
 
-#define RMT_LED_STRIP_RESOLUTION_HZ 10000000 // 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
-#define RMT_LED_STRIP_GPIO_NUM      13
+#define EXAMPLE_CHASE_SPEED_MS 10
 
-#define EXAMPLE_LED_NUMBERS         24
-#define EXAMPLE_CHASE_SPEED_MS      100
-
-    Manager::Manager() {
-        InitRMT();
-    }
+    Manager::Manager() { InitWs2812(); }
 
     std::list<std::string> Manager::GetModeList() {
         std::list<std::string> list;
@@ -29,9 +28,7 @@ namespace light {
         return list;
     }
 
-    std::string Manager::GetMode() {
-        return mode.name;
-    }
+    std::string Manager::GetMode() { return mode.name; }
 
     bool Manager::SetMode(std::string name) {
         auto m = modeList.find(name);
@@ -55,26 +52,20 @@ namespace light {
     }
 
     std::unique_ptr<mode::Config> Manager::GetModeConfig(const std::string &name) {
-
         return std::make_unique<mode::Config>();
     }
 
-    bool Manager::SetModeConfig(std::string name, mode::Config &config) {
-        return false;
-    }
+    bool Manager::SetModeConfig(std::string name, mode::Config &config) { return false; }
 
-    void RenderNext(void *m) {
+    [[noreturn]] void RenderNext(void *m) {
         auto mode = (mode::Mode *) m;
 
         while (true) {
             mode->CalculateNext();
 
-            manager()->TxRMT();
+            manager()->TxWs2812();
 
             vTaskDelay(pdMS_TO_TICKS(EXAMPLE_CHASE_SPEED_MS));
-
-            // TODO: 设置引脚
-            // https://github.com/espressif/esp-idf/blob/master/examples/peripherals/rmt/led_strip/main/led_strip_example_main.c
         }
     }
 
@@ -92,59 +83,14 @@ namespace light {
     }
 
     void Manager::RegisterMode(const std::string &name, mode::Creator c, mode::Destroyer d) {
-        modeList[name] = ModeInfo{
-                name,
-                c,
-                d
-        };
+        modeList[name] = ModeInfo{name, c, d};
 
         ESP_LOGI(MODULE, "Light Mode[%s] Registered", name.c_str());
     }
 
-    void Manager::InitRMT() {
-        ESP_LOGI(MODULE, "Create RMT TX channel");
-        rmt_tx_channel_config_t tx_chan_config = {
-                .gpio_num = RMT_LED_STRIP_GPIO_NUM,
-                .clk_src = RMT_CLK_SRC_DEFAULT, // select source clock
-                .resolution_hz = RMT_LED_STRIP_RESOLUTION_HZ,
-                .mem_block_symbols = 512, // increase the block size can make the LED less flickering
-                .trans_queue_depth = 4, // set the number of transactions that can be pending in the background
-        };
-        ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_chan_config, &rmt.led_chan));
+    void Manager::InitWs2812() { initSPIws2812(); }
 
-        ESP_LOGI(MODULE, "Install led strip encoder");
-        led_strip_encoder_config_t encoder_config = {
-                .resolution = RMT_LED_STRIP_RESOLUTION_HZ,
-        };
-        ESP_ERROR_CHECK(rmt_new_led_strip_encoder(&encoder_config, &rmt.led_encoder));
-
-        ESP_LOGI(MODULE, "Enable RMT TX channel");
-        ESP_ERROR_CHECK(rmt_enable(rmt.led_chan));
-
-        ESP_LOGI(MODULE, "Start LED rainbow chase");
-//        rmt_transmit_config_t tx_config = {
-//                .loop_count = 0, // no transfer loop
-//        };
-    }
-
-    const float POWER_LIMIT = 1;
-
-    void Manager::TxRMT() {
-
-        // int i = 0;
-        // ESP_LOGI(MODULE, "buffer %d %2x%2x%2x", i / 3, lightBuffer.inside[i], lightBuffer.inside[i+1], lightBuffer.inside[i+2]);
-
-        uint8_t buf[LIGHT_INSIDE_POINTS * 3];
-
-        for (int i = 0; i < LIGHT_INSIDE_POINTS * 3; i++) {
-            buf[i] = lightBuffer.inside[i] * POWER_LIMIT;
-        }
-
-        // Flush RGB values to LEDs
-        ESP_ERROR_CHECK(rmt_transmit(rmt.led_chan, rmt.led_encoder, buf, sizeof(lightBuffer.inside), &rmt.tx_config));
-
-    }
-
+    void Manager::TxWs2812() { led_strip_update(lightBuffer.inside); }
 
     Manager *manager() {
         static class Manager *m = nullptr;
@@ -154,4 +100,4 @@ namespace light {
 
         return m;
     }
-}
+}  // namespace light
