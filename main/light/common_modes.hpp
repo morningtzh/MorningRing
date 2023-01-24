5d2a02954c109125084dee12352fed4e6608721b
MorningTZH <morningtzh@yeah.net>
2023-01-24 22:41:33 +0800

    api/配置/持久化解决

--- main/light/common_modes.hpp
+++ main/light/common_modes.hpp
@@ -7,79 +7,82 @@
 
 namespace light::mode {
 
-static const char *MODULE = "Light";
+    static const char *MODULE = "LightMode";
 
 /***************** HSVRing Mode *************/
-class HSVRing : public Mode {
-   public:
-    explicit HSVRing(LightBuffer &lightBuffer) : Mode(lightBuffer) {
-        for (int i = 0; i < LIGHT_INSIDE_POINTS; i++) {
-            lightBuffer.SetHSV(INSIDE_RING, i, HSV_HUE_MAX * i / LIGHT_INSIDE_POINTS, 255, 255);
+    class HSVRing : public Mode {
+
+        static Config defaultConfig;
+
+    public:
+        explicit HSVRing(LightBuffer &lightBuffer, bool start = true) : Mode(lightBuffer, "HSVRing", defaultConfig) {
+            if (start) {
+                for (int i = 0; i < LIGHT_INSIDE_POINTS; i++) {
+                    lightBuffer.SetHSV(INSIDE_RING, i, HSV_HUE_MAX * i / LIGHT_INSIDE_POINTS, 255, 255);
+                }
+
+                for (int i = LIGHT_OUTSIDE_POINTS - 1; i >= 0; i--) {
+                    lightBuffer.SetHSV(OUTSIDE_RING, i, HSV_HUE_MAX * i / LIGHT_OUTSIDE_POINTS, 255, 255);
+                }
+            }
         }
 
-        for (int i = LIGHT_OUTSIDE_POINTS - 1; i >= 0; i--) {
-            lightBuffer.SetHSV(OUTSIDE_RING, i, HSV_HUE_MAX * i / LIGHT_OUTSIDE_POINTS, 255, 255);
+        void CalculateNext() override {
+            lightBuffer.Rotate(INSIDE_RING, GetConfig("InsideRingDirect"), GetConfig("InsideRingStep"));
+            lightBuffer.Rotate(OUTSIDE_RING, GetConfig("OutsideRingDirect"), GetConfig("OutsideRingStep"));
         }
-    }
+    };
 
-    void CalculateNext() override {
-        lightBuffer.Rotate(INSIDE_RING, CLOCKWISE, 1);
-        lightBuffer.Rotate(OUTSIDE_RING, COUNTERCLOCKWISE, 2);
-    }
-};
+    Config HSVRing::defaultConfig{
+            {"InsideRingDirect", {CLOCKWISE}},
+            {"OutsideRingDirect", {COUNTERCLOCKWISE}},
+            {"InsideRingStep", {1}},
+            {"OutsideRingStep", {2}},
+    };
 
 /***************** Test Mode *************/
-class Test : public Mode {
-   private:
-    int hue = 0;
+    class Test : public Mode {
+    private:
+        int hue = 0;
 
-   public:
-    explicit Test(LightBuffer &lightBuffer) : Mode(lightBuffer) {
-        for (int i = 0; i < LIGHT_INSIDE_POINTS; i++) {
-            lightBuffer.SetHSV(INSIDE_RING, i, hue, 255, 255);
+    public:
+        explicit Test(LightBuffer &lightBuffer, bool start) : Mode(lightBuffer) {
+            for (int i = 0; i < LIGHT_INSIDE_POINTS; i++) {
+                lightBuffer.SetHSV(INSIDE_RING, i, hue, 255, 255);
+            }
         }
 
-        // int i = 0;
-        // ESP_LOGI(MODULE, "HSVRing Start buffer %d %2x%2x%2x", i / 3, lightBuffer.inside[i], lightBuffer.inside[i+1],
-        // lightBuffer.inside[i+2]);
-    }
-
-    void CalculateNext() override {
-        for (int i = 0; i < LIGHT_INSIDE_POINTS; i++) {
-            lightBuffer.SetHSV(INSIDE_RING, i, hue++, 255, 255);
+        void CalculateNext() override {
+            for (int i = 0; i < LIGHT_INSIDE_POINTS; i++) {
+                lightBuffer.SetHSV(INSIDE_RING, i, hue++, 255, 255);
+            }
         }
-    }
-};
+    };
 
-class Single : public Mode {
-   private:
-    int rgb = 0;
-    bool dir = true;
+    class Single : public Mode {
+    private:
+        int rgb = 0;
+        bool dir = true;
 
-   public:
-    explicit Single(LightBuffer &lightBuffer) : Mode(lightBuffer) {
-        for (int i = 0; i < LIGHT_INSIDE_POINTS; i++) {
-            lightBuffer.SetRGB(INSIDE_RING, i, 0, 0, 0);
+    public:
+        explicit Single(LightBuffer &lightBuffer, bool start) : Mode(lightBuffer) {
+            if (start) {
+                for (int i = 0; i < LIGHT_INSIDE_POINTS; i++) {
+                    lightBuffer.SetRGB(INSIDE_RING, i, 0, 0, 0);
+                }
+            }
         }
 
-        // int i = 0;
-        // ESP_LOGI(MODULE, "HSVRing Start buffer %d %2x%2x%2x", i / 3, lightBuffer.inside[i], lightBuffer.inside[i+1],
-        // lightBuffer.inside[i+2]);
-    }
+        void CalculateNext() override {
 
-    void CalculateNext() override {
-        rgb++;
-        if (rgb > 50) {
-            rgb = 0;
+            rgb++;
+            if (rgb > 255) {
+                rgb = 0;
+            }
+
+            for (int i = 0; i < LIGHT_INSIDE_POINTS; i++) {
+                lightBuffer.SetRGB(INSIDE_RING, i, rgb, rgb, rgb);
+            }
         }
-
-        for (int i = 0; i < LIGHT_INSIDE_POINTS; i++) {
-            lightBuffer.SetRGB(INSIDE_RING, i, rgb, rgb, rgb);
-        }
-
-        // int i = 0;
-        // ESP_LOGI(MODULE, "HSVRing buffer %d %2x%2x%2x", i / 3, lightBuffer.inside[i], lightBuffer.inside[i+1],
-        // lightBuffer.inside[i+2]);
-    }
-};
+    };
 }  // namespace light::mode
--- main/light/mode.hpp
+++ main/light/mode.hpp
@@ -10,47 +10,59 @@
 #include <list>
 #include <memory>
 #include <functional>
+#include <utility>
+#include <nvs_flash.h>
 #include "common.hpp"
 #include "fast_hsv2rgb.h"
 
 class string;
 namespace light::mode {
-    using Config = std::map<std::string, int>;
+
+    struct ConfigItem {
+        int32_t value;
+    };
+
+    using Config = std::map<std::string, ConfigItem>;
 
     class Mode {
     private:
+
+        bool isSupportConfig = false;
+        std::string configPrefix;
         Config config;
+
         bool dirty = true;
 
     protected:
         LightBuffer &lightBuffer;
     public:
-        Mode(LightBuffer &lightBuffer) : lightBuffer(lightBuffer) {};
+        Mode(LightBuffer &lightBuffer);
 
-        virtual bool SetConfig(Config &newConfig) {
-            return true;
-        };
+        Mode(LightBuffer &lightBuffer, std::string configPrefix, const Config& defaultConfig);
 
-        virtual std::unique_ptr<Config> GetConfig() {
-            return std::make_unique<Config>();
-        };
+        virtual ~Mode();
+
+        void InitConfig(const Config &defaultConfig);
+
+        virtual bool SetConfig(Config &newConfig);
+
+        virtual std::unique_ptr<Config> GetConfig();
+
+        int GetConfig(std::string key);
 
         virtual void CalculateNext() = 0;
 
-        virtual ~Mode() = default;
-
     };
 
-    using Creator = std::function<Mode *(light::LightBuffer &)>;
+    using Creator = std::function<Mode *(light::LightBuffer &, bool start)>;
     //typedef Mode *(*Creator)();
 
     typedef void (*Destroyer)(Mode *);
 
     template<typename T>
-    Mode *CreateMode(light::LightBuffer &lightBuffer) {
-        return new T(lightBuffer);
+    Mode *CreateMode(light::LightBuffer &lightBuffer, bool start = true) {
+        return new T(lightBuffer, start);
     };
 }
-
 
 #endif //MORNINGRING_MODE_HPP
